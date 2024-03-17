035f98e0684317dcfb050c58bf2104fd8645b0c7
raya <amiyou@LinUxTo5re>
2024-03-17 20:45:23 +0530

    updated tests

--- ORMapp/tests.py
+++ ORMapp/tests.py
@@ -2,13 +2,13 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from .models import BooksLanguage, BooksBook, BooksSubject, BooksBookshelf, BooksAuthor
 
+# TDD-> checking test cases (removed unwated)
 class TestGutenbergDataListView(TestCase):
     def test_gutenberg_data_list_view_with_invalid_language(self):
         client = Client()
         url = reverse('gb-list')
         response = client.get(url, {'language': ['en','fr']})
         self.assertEqual(response.status_code, 200)
-
 
     def test_gutenberg_data_list_view_with_multiple_valid_ids(self):
         client = Client()
